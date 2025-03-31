2de9071a1966cb8088462465de3d50155c09ec83
pfollis618 <pfollis618@gmail.com>
2025-03-31 12:03:38 -0500

    Update script.js

--- Chapter3Project/JS/script.js
+++ Chapter3Project/JS/script.js
@@ -4,3 +4,11 @@ const salary = 100000;
 console.log(salary);
 const veteran = "not a veteran";
 console.log(veteran);
+const friends = array("James", "Dawson", "Mary");
+console.log(friends);
+const friendSalary = array();
+console.log(friendSalary);
+var ben = {
+  name: "Ben Smith",
+  salary: 90000
+}; 
